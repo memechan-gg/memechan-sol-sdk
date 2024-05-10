6b31641b89fba7e635b20d779f198f7c855c1700
Tamas Szucs <0xunreal@proton.me>
2024-05-10 17:50:40 +0200

    wip: swap test

--- test/BoundPool.test.ts
+++ test/BoundPool.test.ts
@@ -43,4 +43,42 @@ describe("BoundPool", () => {
         })
 
     }, 120000);
+
+    it("swaps full sol->memecoin in one go", async () => {
+       const admin = new PublicKey(process.env.ADMIN_PUB_KEY as string);
+        const payer =  Keypair.fromSecretKey(Buffer.from(JSON.parse(process.env.TEST_USER_SECRET_KEY as string)));
+        const wallet = new NodeWallet(payer);
+        const client = new MemechanClient(wallet);
+
+        const pool = await BoundPool.new({admin, payer, signer: payer, client });
+
+        await sleep(1000);
+
+      // call to the swap endpoint
+      const ticketId = await pool.swapY({
+        memeTokensOut: new BN(1),
+        solAmountIn: new BN(2 * 1e9),
+      });
+
+      sleep(1000);
+
+      const poolInfo = await pool.fetch();
+
+      expect(poolInfo.locked).toBe(true);
+
+      const ticketInfo = await ticketId.fetch();
+
+      const memesTotal = ticketInfo.amount.add(poolInfo.adminFeesMeme);
+      expect(memesTotal).toBe(new BN(9e14)); //"total sum of memetokens with fees should amount to 9e14")
+
+      const solAmt = poolInfo.solReserve.tokens;
+      expect(solAmt).toBe(new BN(1 * 1e9)); // "pool should have 300 sol")
+
+      const solVault = await client.connection.getAccountInfo(
+        poolInfo.solReserve.vault,
+      )
+
+      const totalAmt = new BN(solVault?.lamports) - poolInfo.adminFeesSol;
+      expect(totalAmt).toBe(BigInt(2e11)); //, "pool should have 2 sol without admin fees")
+    }, 120000);
 });
