21f9b63e64bb5230924ff566be4d0474ed4fb381
Tamas Szucs <0xunreal@proton.me>
2024-05-16 10:46:51 +0200

    cleanup

--- src/raydium/getAmmConfigId.ts
+++
@@ -1,7 +0,0 @@
-import { PublicKey } from "@solana/web3.js"
-import { findProgramAddress } from "../common/helpers"
-
-  export function getAmmConfigId(programId : PublicKey) {
-    const { publicKey } = findProgramAddress([Buffer.from('amm_config_account_seed', 'utf-8')], programId)
-    return publicKey
-  }
