21f356d0371e8bdc93c99475172c6ea0f2fef769
avernikoz <avernikoz@gmail.com>
2024-05-17 20:45:58 +0300

    remove unused env

--- env.d.ts
+++ env.d.ts
@@ -4,10 +4,8 @@ declare namespace NodeJS {
     TEST_PAYER_SECRET_KEY: string | undefined;
     TEST_USER_SECRET_KEY: string | undefined;
     ADMIN_PUB_KEY: string | undefined;
-    MEMECHAN_PROGRAM_ID: string | undefined;
     RPC_API_CLUSTER: string | undefined;
     WSS_API_CLUSTER: string | undefined;
-    BE_URL: string | undefined;
     FEE_DESTINATION_ID: string | undefined;
     USER_ID: string | undefined;
   }
