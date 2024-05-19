9d9e859d44f7ce6e29e8b2d506c4617bd5a07481
avernikoz <avernikoz@gmail.com>
2024-05-19 12:33:29 +0300

    update tests

--- test/StakingPool.test.ts
+++ test/StakingPool.test.ts
@@ -1,21 +1,8 @@
-import { Keypair, PublicKey } from "@solana/web3.js";
-import { BoundPool } from "../src/bound-pool/BoundPool";
-import { airdrop, sleep } from "../src/common/helpers";
-import { BN } from "@coral-xyz/anchor";
-import { MemechanClient } from "../src/MemechanClient";
-import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
-import { createWrappedNativeAccount, getAccount } from "@solana/spl-token";
-import { MemeTicket } from "../src/memeticket/MemeTicket";
 import { StakingPool } from "../src/staking-pool/StakingPool";
+import { client } from "./common/common";
 
 describe("StakingPool", () => {
-
-  it("all", async() => {
-    const payer = Keypair.fromSecretKey(Buffer.from(JSON.parse(process.env.TEST_USER_SECRET_KEY as string)));
-    console.log("payer: " + payer.publicKey.toString());
-
-    const wallet = new NodeWallet(payer);
-    const client = new MemechanClient(wallet);
+  it("all", async () => {
     const all = await StakingPool.all(client.memechanProgram);
 
     for (const pool of all) {
