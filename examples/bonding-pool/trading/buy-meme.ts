11cd7bbaf78ddd012dc9fa1a480229850de61e77
avernikoz <avernikoz@gmail.com>
2024-05-20 19:11:00 +0300

    upd buy meme

--- examples/bonding-pool/trading/buy-meme.ts
+++ examples/bonding-pool/trading/buy-meme.ts
@@ -1,6 +1,11 @@
+import { PublicKey } from "@solana/web3.js";
 import { BoundPoolClient } from "../../../src/bound-pool/BoundPool";
+import { client } from "../../common";
 
 // yarn tsx examples/bonding-pool/trading/buy-meme.ts
 (async () => {
-  // new BoundPool({ });
+  const poolAccountAddressId = new PublicKey("9pF4n8h8r4hDZftZffoHfvEaox9aZCqh3n4WTCXS5HCE");
+  const boundPoolInstance = await BoundPoolClient.fromBoundPoolId({ client, poolAccountAddressId });
+
+  console.debug("boundPoolInstance: ", boundPoolInstance);
 })();
