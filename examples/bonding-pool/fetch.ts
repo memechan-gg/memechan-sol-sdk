4615edd3edf9e43ce9a84f7b7eb27d31d5c94655
avernikoz <avernikoz@gmail.com>
2024-05-20 17:46:07 +0300

    fix fetch

--- examples/bonding-pool/fetch.ts
+++ examples/bonding-pool/fetch.ts
@@ -1,12 +1,12 @@
 import { PublicKey } from "@solana/web3.js";
-import { BoundPool } from "../../src/bound-pool/BoundPool";
+import { BoundPoolClient } from "../../src/bound-pool/BoundPool";
 import { connection } from "../common";
 
 // yarn tsx examples/bonding-pool/fetch.ts > log.txt 2>&1
 export async function fetch() {
   const poolAddress = new PublicKey("GFC6FrfkSP12k5h31Cs7zfpBAaspnAfmYdciS5Pfwt2J");
 
-  const pool = await BoundPool.fetch2(connection, poolAddress);
+  const pool = await BoundPoolClient.fetch2(connection, poolAddress);
   console.log("pool:", pool);
 }
 
