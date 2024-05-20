d64e76a2b572d7922d69641fbe247b175a9a9d63
avernikoz <avernikoz@gmail.com>
2024-05-20 17:47:21 +0300

    fix import

--- examples/bonding-pool/trading/buy-meme.ts
+++ examples/bonding-pool/trading/buy-meme.ts
@@ -1,4 +1,4 @@
-import { BoundPool } from "../../../src/bound-pool/BoundPool";
+import { BoundPoolClient } from "../../../src/bound-pool/BoundPool";
 
 // yarn tsx examples/bonding-pool/trading/buy-meme.ts
 (async () => {
