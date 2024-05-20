c6eaa446bfc08e113f06f72b57b67c5d067a194c
bathord <alutskin@bk.ru>
2024-05-20 14:12:58 +0300

    Upd fetch example

--- examples/bonding-pool/fetch.ts
+++ examples/bonding-pool/fetch.ts
@@ -5,7 +5,8 @@ import { connection } from "../common";
 // yarn tsx examples/bonding-pool/fetch.ts > log.txt 2>&1
 export async function fetch() {
   const poolAddress = new PublicKey("GFC6FrfkSP12k5h31Cs7zfpBAaspnAfmYdciS5Pfwt2J");
-  const pool = await BoundPool.fetch(connection, poolAddress);
+
+  const pool = await BoundPool.fetch2(connection, poolAddress);
   console.log("pool:", pool);
 }
 
