beab41fc98569fdcde0b76c08023ea0039857dd7
0xunreal <0xunreal@proton.me>
2024-05-20 23:50:02 +0200

    add test: close ticket

--- test/MemeTicket.test.ts
+++ test/MemeTicket.test.ts
@@ -5,6 +5,7 @@ import { MemeTicket } from "../src/memeticket/MemeTicket";
 import { DUMMY_TOKEN_METADATA, LIVE_BOUND_POOL_ID, admin, client, payer } from "./common/common";
 import { MEMECHAN_QUOTE_TOKEN } from "../src/config/config";
 import { FEE_DESTINATION_ID } from "./common/env";
+import { MintUtils } from "../src/token/mintUtils";
 
 describe("MemeTicket", () => {
   it.skip("all", async () => {
@@ -73,7 +74,7 @@ describe("MemeTicket", () => {
       console.log("mergedTicket2: " + mergedTicket2.id.toBase58());
     }, 220000);
 
-    it("merge tickets live", async () => {
+    it.skip("merge tickets live", async () => {
       const pool = await BoundPoolClient.slowNew({
         admin,
         payer,
@@ -161,4 +162,45 @@ describe("MemeTicket", () => {
 
       console.log("mergedTicket2: " + mergedTicket2.id.toBase58());
     }, 520000);
+
+    it("close ticket", async () => {
+      const pool = await BoundPoolClient.fromBoundPoolId({client, poolAccountAddressId: LIVE_BOUND_POOL_ID});
+
+      const ticket = await pool.swapY({
+        payer: payer,
+        user: payer,
+        memeTokensOut: new BN(1),
+        quoteAmountIn: new BN(1000),
+        quoteMint: MEMECHAN_QUOTE_TOKEN.mint,
+        pool: pool.id,
+      });
+
+      console.log("ticket: " + ticket.id.toBase58());
+
+      const ticketInfo = await ticket.fetch();
+
+      console.log("ticketInfo: ", ticketInfo);
+
+      const mintUtils = new MintUtils(client.connection, payer);
+      const quoteAccount = await mintUtils.getOrCreateTokenAccount(MEMECHAN_QUOTE_TOKEN.mint, payer, payer.publicKey );
+
+      await sleep(5000);
+
+      const swapXTxResult = await pool.swapX({
+        user: payer,
+        memeAmountIn: ticketInfo.amount,
+        minQuoteAmountOut: new BN(1),
+        quoteMint: MEMECHAN_QUOTE_TOKEN.mint,
+        userMemeTicket: ticket,
+        userQuoteAcc: quoteAccount.address,
+      })
+
+      console.log("swapXTxResult: " + swapXTxResult);
+        
+      const closedTicket = await ticket.close({
+        user: payer
+      });
+
+      console.log("closedTicket: " + closedTicket.id.toBase58());
+    }, 220000);
 });
