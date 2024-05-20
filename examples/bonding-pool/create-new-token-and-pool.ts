2b6d7609fc98dd7dfade7eaa7c005acff5898e1f
bathord <alutskin@bk.ru>
2024-05-20 13:58:55 +0300

    Create example for new pool and coin creation

--- examples/bonding-pool/create-new-token-and-pool.ts
+++ examples/bonding-pool/create-new-token-and-pool.ts
@@ -1,1 +1,28 @@
-export const createNewTokenAndPool = async () => {};
+import { BoundPool } from "../../src/bound-pool/BoundPool";
+import { MEMECHAN_QUOTE_TOKEN } from "../../src/config/config";
+import { admin, payer, client } from "../common";
+
+const DUMMY_TOKEN_METADATA = {
+  name: "Best Token Ever",
+  symbol: "BTE",
+  image: "https://cf-ipfs.com/ipfs/QmVevMfxFpfgBu5kHuYUPmDMaV6pWkAn3zw5XaCXxKdaBh",
+  description: "This is the best token ever",
+  twitter: "https://twitter.com/BestTokenEver",
+  telegram: "https://t.me/BestTokenEver",
+  website: "https://besttokenever.com",
+};
+
+// yarn tsx examples/bonding-pool/create-new-token-and-pool.ts > log.txt 2>&1
+export const createNewTokenAndPool = async () => {
+  const boundPool = await BoundPool.slowNew({
+    admin,
+    payer,
+    signer: payer,
+    client,
+    quoteToken: MEMECHAN_QUOTE_TOKEN,
+    tokenMetadata: DUMMY_TOKEN_METADATA,
+  });
+  console.log("boundPool:", boundPool);
+};
+
+createNewTokenAndPool();
