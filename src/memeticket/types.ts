126e8974f9c288b744dc40bfc81515cf68f5e893
bathord <alutskin@bk.ru>
2024-05-21 20:24:07 +0300

    Create StringifiedMemeTicketFields type

--- src/memeticket/types.ts
+++ src/memeticket/types.ts
@@ -1,4 +1,5 @@
 import { Keypair, PublicKey } from "@solana/web3.js";
+import { MemeTicketFields } from "../schema/codegen/accounts";
 import { MemeTicket } from "./MemeTicket";
 
 export interface BoundMerge {
@@ -16,3 +17,8 @@ export interface StakingMerge {
 export interface CloseArgs {
   user: Keypair;
 }
+
+export type StringifiedMemeTicketFields = Omit<MemeTicketFields, "amount" | "untilTimestamp"> & {
+  amount: string;
+  untilTimestamp: string;
+};
