f40a4a6a44f3654208302b87f15cf3a83868fb4c
bathord <alutskin@bk.ru>
2024-05-21 20:04:35 +0300

    Create examples for tickets

---
+++ examples/bonding-pool/get-available-staked-lp-by-owner.ts
@@ -0,0 +1,21 @@
+import { PublicKey } from "@solana/web3.js";
+import { BoundPoolClient } from "../../src/bound-pool/BoundPool";
+import { client, payer } from "../common";
+
+// yarn tsx examples/bonding-pool/get-available-staked-lp-by-owner.ts > available-staked-lp.txt 2>&1
+export const getAvailableStakedLpByOwner = async () => {
+  const poolAddress = new PublicKey("9Qy1JawWD9Ue7rWfCJUwcTDWqSSbS48e8MhqgGsprrcV");
+
+  const tickets = await BoundPoolClient.fetchAvailableTicketsByUser(poolAddress, client, payer.publicKey);
+  const stringifiedTickets = tickets.map((ticket) => {
+    return {
+      ...ticket,
+      amount: ticket.amount.toString(),
+      untilTimestamp: ticket.untilTimestamp.toString(),
+    };
+  });
+
+  console.log("tickets:", stringifiedTickets);
+};
+
+getAvailableStakedLpByOwner();
