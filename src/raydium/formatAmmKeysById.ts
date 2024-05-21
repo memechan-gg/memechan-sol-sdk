9c7067c57c33f48bc5a2c8e4e48082cd5451aaf8
0xunreal <0xunreal@proton.me>
2024-05-21 10:29:03 +0200

    wip: amm swap

--- src/raydium/formatAmmKeysById.ts
+++ src/raydium/formatAmmKeysById.ts
@@ -8,7 +8,7 @@ import {
 } from "@raydium-io/raydium-sdk";
 import { Connection, PublicKey } from "@solana/web3.js";
 import { safeBNToNumber } from "../utils/safeBNToNumber";
-import { MEMECHAN_MEME_TOKEN_DECIMALS } from "../config/config";
+import { MEMECHAN_MEME_TOKEN_DECIMALS, MEMECHAN_QUOTE_TOKEN_DECIMALS } from "../config/config";
 
 export async function formatAmmKeysById(id: string, connection: Connection): Promise<ApiPoolInfoV4> {
   console.log("formatAmmKeysById id: " + id);
@@ -33,8 +33,8 @@ export async function formatAmmKeysById(id: string, connection: Connection): Pro
     baseMint: info.baseMint.toString(),
     quoteMint: info.quoteMint.toString(),
     lpMint: info.lpMint.toString(),
-    baseDecimals: safeBNToNumber(info.baseDecimal),
-    quoteDecimals: safeBNToNumber(info.quoteDecimal),
+    baseDecimals: MEMECHAN_MEME_TOKEN_DECIMALS, //safeBNToNumber(info.baseDecimal),
+    quoteDecimals: MEMECHAN_QUOTE_TOKEN_DECIMALS, //safeBNToNumber(info.quoteDecimal),
     lpDecimals: MEMECHAN_MEME_TOKEN_DECIMALS,//lpMintInfo.decimals,
     version: 4,
     programId: account.owner.toString(),
