52d5d91e6e9a5a0e4c9793424f81055ecb00bc10
avernikoz <avernikoz@gmail.com>
2024-05-21 19:21:20 +0300

    fix example

--- examples/bonding-pool/trading/get-output-amount-for-buy-meme.ts
+++ examples/bonding-pool/trading/get-output-amount-for-buy-meme.ts
@@ -2,7 +2,7 @@ import { PublicKey } from "@solana/web3.js";
 import { BoundPoolClient } from "../../../src/bound-pool/BoundPool";
 import { client, payer } from "../../common";
 
-// yarn tsx examples/bonding-pool/trading/buy-meme.ts
+// yarn tsx examples/bonding-pool/trading/get-output-amount-for-buy-meme.ts
 (async () => {
   const poolAccountAddressId = new PublicKey("HFpBiAWW3uJ3rdT6SvT4xo9A2Pi2uAhNjNnNLmbxjG94");
   const boundPoolInstance = await BoundPoolClient.fromBoundPoolId({ client, poolAccountAddressId });
