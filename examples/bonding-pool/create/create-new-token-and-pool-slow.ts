5b9b51783e0d4c1a65669b49ed6b96ed0b085aca
bathord <alutskin@bk.ru>
2024-05-21 20:25:50 +0300

    Move create examples

--- examples/bonding-pool/create/create-new-token-and-pool-slow.ts
+++ examples/bonding-pool/create/create-new-token-and-pool-slow.ts
@@ -1,6 +1,6 @@
-import { BoundPoolClient } from "../../src/bound-pool/BoundPool";
-import { MEMECHAN_QUOTE_TOKEN } from "../../src/config/config";
-import { admin, payer, client } from "../common";
+import { BoundPoolClient } from "../../../src/bound-pool/BoundPool";
+import { MEMECHAN_QUOTE_TOKEN } from "../../../src/config/config";
+import { admin, payer, client } from "../../common";
 
 const DUMMY_TOKEN_METADATA = {
   name: "Best Token Ever",
