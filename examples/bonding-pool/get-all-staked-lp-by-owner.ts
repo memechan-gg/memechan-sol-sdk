db8b38c4f3f0a8a63ef37fe1641aefaf800782c2
bathord <alutskin@bk.ru>
2024-05-21 20:24:39 +0300

    Move ticket methods from BoundPool to MemeTicket class

--- examples/bonding-pool/get-all-staked-lp-by-owner.ts
+++ examples/bonding-pool/get-all-staked-lp-by-owner.ts
@@ -1,12 +1,12 @@
 import { PublicKey } from "@solana/web3.js";
-import { BoundPoolClient } from "../../src/bound-pool/BoundPool";
+import { MemeTicket } from "../../src/memeticket/MemeTicket";
 import { client, payer } from "../common";
 
 // yarn tsx examples/bonding-pool/get-all-staked-lp-by-owner.ts > all-staked-lp.txt 2>&1
 export const getAllStakedLpByOwner = async () => {
   const poolAddress = new PublicKey("9Qy1JawWD9Ue7rWfCJUwcTDWqSSbS48e8MhqgGsprrcV");
 
-  const tickets = await BoundPoolClient.fetchTicketsByUser(poolAddress, client, payer.publicKey);
+  const tickets = await MemeTicket.fetchTicketsByUser(poolAddress, client, payer.publicKey);
   const stringifiedTickets = tickets.map((ticket) => {
     return {
       ...ticket,
--- src/memeticket/MemeTicket.ts
+++ src/memeticket/MemeTicket.ts
@@ -1,8 +1,9 @@
-import { PublicKey } from "@solana/web3.js";
-import { BoundMerge, CloseArgs, StakingMerge } from "./types";
-import { MemechanClient } from "../MemechanClient";
-import { MemechanSol } from "../schema/types/memechan_sol";
 import { Program } from "@coral-xyz/anchor";
+import { GetProgramAccountsFilter, PublicKey } from "@solana/web3.js";
+import { MemechanClient } from "../MemechanClient";
+import { MemeTicketFields } from "../schema/codegen/accounts";
+import { MemechanSol } from "../schema/types/memechan_sol";
+import { BoundMerge, CloseArgs, StakingMerge, StringifiedMemeTicketFields } from "./types";
 
 export class MemeTicket {
   public constructor(
@@ -70,5 +71,70 @@ export class MemeTicket {
       .rpc();
 
     return this;
+  }
+
+  /**
+   * Fetches all tickets for provided pool id
+   */
+  public static async fetchRelatedTickets(pool: PublicKey, client: MemechanClient): Promise<MemeTicketFields[]> {
+    const program = client.memechanProgram;
+    const filters: GetProgramAccountsFilter[] = [
+      {
+        memcmp: {
+          bytes: pool.toBase58(),
+          offset: 40,
+        },
+      },
+    ];
+
+    const fetchedTickets = await program.account.memeTicket.all(filters);
+    const tickets = fetchedTickets.map((ticket) => ticket.account);
+    return tickets;
+  }
+
+  public static async fetchTicketsByUser(
+    pool: PublicKey,
+    client: MemechanClient,
+    user: PublicKey,
+  ): Promise<StringifiedMemeTicketFields[]> {
+    const program = client.memechanProgram;
+    const filters: GetProgramAccountsFilter[] = [
+      {
+        memcmp: {
+          bytes: pool.toBase58(),
+          offset: 40,
+        },
+      },
+      {
+        memcmp: {
+          bytes: user.toBase58(),
+          offset: 8,
+        },
+      },
+    ];
+
+    const fetchedTickets = await program.account.memeTicket.all(filters);
+    const tickets = fetchedTickets.map((ticket) => ticket.account);
+
+    const stringifiedTickets = tickets.map((ticket) => {
+      return {
+        ...ticket,
+        amount: ticket.amount.toString(),
+        untilTimestamp: ticket.untilTimestamp.toString(),
+      };
+    });
+
+    return stringifiedTickets;
+  }
+
+  public static async fetchAvailableTicketsByUser(pool: PublicKey, client: MemechanClient, user: PublicKey) {
+    const tickets = await MemeTicket.fetchTicketsByUser(pool, client, user);
+
+    return tickets.filter((ticket) => {
+      const currentTimestamp = Date.now();
+      const unlockTicketTimestamp = +ticket.untilTimestamp;
+
+      return currentTimestamp >= unlockTicketTimestamp;
+    });
   }
 }
