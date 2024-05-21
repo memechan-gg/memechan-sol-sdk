8696e54345c191995eff9cf13979107c4e4b3c92
bathord <alutskin@bk.ru>
2024-05-21 20:27:03 +0300

    Fix examples

--- examples/meme-ticket/get-all-staked-lp-by-owner.ts
+++ examples/meme-ticket/get-all-staked-lp-by-owner.ts
@@ -2,7 +2,7 @@ import { PublicKey } from "@solana/web3.js";
 import { MemeTicket } from "../../src/memeticket/MemeTicket";
 import { client, payer } from "../common";
 
-// yarn tsx examples/bonding-pool/get-all-staked-lp-by-owner.ts > all-staked-lp.txt 2>&1
+// yarn tsx examples/meme-ticket/get-all-staked-lp-by-owner.ts > all-staked-lp.txt 2>&1
 export const getAllStakedLpByOwner = async () => {
   const poolAddress = new PublicKey("9Qy1JawWD9Ue7rWfCJUwcTDWqSSbS48e8MhqgGsprrcV");
 
--- examples/meme-ticket/get-available-staked-lp-by-owner.ts
+++ examples/meme-ticket/get-available-staked-lp-by-owner.ts
@@ -2,7 +2,7 @@ import { PublicKey } from "@solana/web3.js";
 import { MemeTicket } from "../../src/memeticket/MemeTicket";
 import { client, payer } from "../common";
 
-// yarn tsx examples/bonding-pool/get-available-staked-lp-by-owner.ts > available-staked-lp.txt 2>&1
+// yarn tsx examples/meme-ticket/get-available-staked-lp-by-owner.ts > available-staked-lp.txt 2>&1
 export const getAvailableStakedLpByOwner = async () => {
   const poolAddress = new PublicKey("9Qy1JawWD9Ue7rWfCJUwcTDWqSSbS48e8MhqgGsprrcV");
 
