c466d400b52cac360a81645aa13172d548e3fa57
avernikoz <avernikoz@gmail.com>
2024-05-21 20:17:46 +0300

    fix test

--- test/TargetConfig.test.ts
+++ test/TargetConfig.test.ts
@@ -7,7 +7,6 @@ import { MintUtils } from "../src/token/mintUtils";
 
 describe("TargetConfig", () => {
   it.skip("creates target config", async () => {
-
     const mintUtils = new MintUtils(client.connection, payer);
     const mint = await mintUtils.createMint(MEMECHAN_MEME_TOKEN_DECIMALS);
 
@@ -27,19 +26,18 @@ describe("TargetConfig", () => {
     console.log(info);
   }, 90000);
   it.skip("change_target_config", async () => {
-    
-    const targetConfig = await TargetConfig.fromTargetConfigId({client, accountAddressId: MEMECHAN_TARGET_CONFIG});
+    const targetConfig = await TargetConfig.fromTargetConfigId({ client, accountAddressId: MEMECHAN_TARGET_CONFIG });
 
     console.log("original targetConfig: ", targetConfig.tokenTargetAmount.toString());
     const newTargetAmount = targetConfig.tokenTargetAmount.add(new BN(10));
     console.log("new target amount: ", newTargetAmount.toString());
 
-    await targetConfig.changeTargetConfig(newTargetAmount);
-  
+    await targetConfig.changeTargetConfig(newTargetAmount, payer);
+
     await sleep(5000);
 
     const targetConfig2 = await TargetConfig.fetch(client.connection, targetConfig.id);
     console.log("changed targetConfig: ", targetConfig2.tokenTargetAmount.toString());
     expect(targetConfig2.tokenTargetAmount).toBe(newTargetAmount);
-  }, 20000)
+  }, 20000);
 });
