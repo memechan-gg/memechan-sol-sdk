98979b8311881bd32cdc1a89bb2458caace18d53
bathord <alutskin@bk.ru>
2024-05-22 17:30:17 +0300

    Rename raydium examples folder to live pool

--- examples/live-pool/get-meme-price-and-mcap.ts
+++ examples/live-pool/get-meme-price-and-mcap.ts
@@ -2,7 +2,7 @@ import { BoundPoolClient } from "../../src/bound-pool/BoundPool";
 import { LivePool } from "../../src/live-pool/LivePool";
 import { connection } from "../common";
 
-// yarn tsx examples/raydium/get-meme-price-and-mcap.ts > log.txt 2>&1
+// yarn tsx examples/live-pool/get-meme-price-and-mcap.ts > log.txt 2>&1
 export const getMemePriceAndMarketCap = async () => {
   const poolAddress = "BY6xstuufxC7sii4iqYXToSzYrT8wBcLkrwrVatHXkQs";
   const memePrice = await LivePool.getMemePrice({ poolAddress, connection, quotePriceInUsd: 0.31 });
--- examples/live-pool/get-pool-info.ts
+++ examples/live-pool/get-pool-info.ts
@@ -2,7 +2,7 @@ import { jsonInfo2PoolKeys } from "@raydium-io/raydium-sdk";
 import { formatAmmKeysById } from "../../src/raydium/formatAmmKeysById";
 import { connection } from "../common";
 
-// yarn tsx examples/raydium/get-pool-info.ts > log.txt 2>&1
+// yarn tsx examples/live-pool/get-pool-info.ts > log.txt 2>&1
 export const getPoolInfo = async () => {
   const poolAddress = "BY6xstuufxC7sii4iqYXToSzYrT8wBcLkrwrVatHXkQs";
 
