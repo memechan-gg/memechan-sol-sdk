0c41f67703286033aaf4bd20b84166a2b5eb8854
Tamas Szucs <0xunreal@proton.me>
2024-05-22 13:34:31 +0200

    add withdrawFees test

--- test/StakingPool.test.ts
+++ test/StakingPool.test.ts
@@ -22,7 +22,7 @@ describe("StakingPool", () => {
     }
   }, 30000);
 
-  it("swapy, golive, ammSwap, unstake", async () => {
+  it.skip("swapy, golive, ammSwap, unstake", async () => {
     const boundPool = await BoundPoolClient.slowNew({
       admin,
       payer,
@@ -109,6 +109,95 @@ describe("StakingPool", () => {
     });
 
     console.log("unstakeResult: ", unstakeResult);
+
+  }, 550000);
+
+  it("swapy, golive, ammSwap, withdrawfees", async () => {
+    const boundPool = await BoundPoolClient.slowNew({
+      admin,
+      payer,
+      signer: payer,
+      client,
+      quoteToken: MEMECHAN_QUOTE_TOKEN,
+      tokenMetadata: DUMMY_TOKEN_METADATA,
+    });
+
+    console.log("==== pool id: " + boundPool.id.toString());
+    await sleep(2000);
+    
+    const tickets: MemeTicket[] = [];
+
+    const ticketId = await boundPool.swapY({
+      payer: payer,
+      user: payer,
+      memeTokensOut: new BN(100),
+      quoteAmountIn: new BN(1000),
+      quoteMint: MEMECHAN_QUOTE_TOKEN.mint,
+      pool: boundPool.id,
+    });
+
+    tickets.push(new MemeTicket(ticketId.id, client));
+    console.log("swapY ticketId: " + ticketId.id.toBase58());
+
+    const ticketId2 = await boundPool.swapY({
+      payer: payer,
+      user: payer,
+      memeTokensOut: new BN(1000),
+      quoteAmountIn: new BN(100000),
+      quoteMint: MEMECHAN_QUOTE_TOKEN.mint,
+      pool: boundPool.id,
+    });
+
+    tickets.push(new MemeTicket(ticketId2.id, client));
+    console.log("swapY ticketId2: " + ticketId2.id.toBase58());
+
+    const boundPoolInfo = await BoundPoolClient.fetch2(client.connection, boundPool.id);
+
+    console.log("boundPoolInfo:", boundPoolInfo);
+
+    const { stakingMemeVault, stakingQuoteVault } = await boundPool.slowInitStakingPool({
+      payer: payer,
+      user: payer,
+      boundPoolInfo,
+    });
+
+    const [stakingPool, ammPool ] = await boundPool.goLive({
+      payer: payer,
+      user: payer,
+      boundPoolInfo,
+      feeDestinationWalletAddress: FEE_DESTINATION_ID,
+      memeVault: stakingMemeVault,
+      quoteVault: stakingQuoteVault,
+    });
+    console.log("ammPool: " + JSON.stringify(ammPool));
+
+    const inputToken = MEMECHAN_QUOTE_TOKEN;
+    const outputToken = new Token(TOKEN_PROGRAM_ID, ammPool.baseMint, MEMECHAN_MEME_TOKEN_DECIMALS)
+    const inputTokenAmount = new TokenAmount(inputToken, 10000)
+    const slippage = new Percent(5, 100)
+    const walletTokenAccounts = await getWalletTokenAccount(client.connection, payer.publicKey)
+
+    const swapTxIds = await swapOnlyAmm({
+      connection: client.connection,
+      outputToken,
+      targetPool: ammPool.id,
+      inputTokenAmount,
+      slippage,
+      walletTokenAccounts,
+      wallet: payer,
+    });
+
+    console.log('amm swapresult txids: ', swapTxIds)
+
+
+    StakingPool.fromStakingPoolId({ client, poolAccountAddressId: stakingPool.id});
+
+    const withrdawResult = await stakingPool.withdrawFees({
+      user: payer,
+      ticket: tickets[0],
+    });
+
+    console.log("withrdawResult: ", withrdawResult);
 
   }, 550000);
 });
