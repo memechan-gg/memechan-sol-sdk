2c141e2ae7da97c79d1938c43b9402a1147018dc
byte43 <byte43@proton.me>
2024-05-22 21:01:16 +0200

    fix: export bound pool

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,4 @@ export * from "./tx-parsing/parsers/init-staking-parser";
 export * from "./tx-parsing/parsers/swap-x-parser";
 export * from "./tx-parsing/parsers/swap-y-parser";
 export * from './tx-parsing/parsers/create-metadata-parser';
+export * from './bound-pool/BoundPool';
