9167730bda8216cd8878fb2e763b34c26b7bc822
Tamas Szucs <0xunreal@proton.me>
2024-05-22 13:21:35 +0200

    unstake test

--- test/StakingPool.test.ts
+++ test/StakingPool.test.ts
@@ -34,6 +34,32 @@ describe("StakingPool", () => {
 
     console.log("==== pool id: " + boundPool.id.toString());
     await sleep(2000);
+    
+    const tickets: MemeTicket[] = [];
+
+    const ticketId = await boundPool.swapY({
+      payer: payer,
+      user: payer,
+      memeTokensOut: new BN(100),
+      quoteAmountIn: new BN(1000),
+      quoteMint: MEMECHAN_QUOTE_TOKEN.mint,
+      pool: boundPool.id,
+    });
+
+    tickets.push(new MemeTicket(ticketId.id, client));
+    console.log("swapY ticketId: " + ticketId.id.toBase58());
+
+    const ticketId2 = await boundPool.swapY({
+      payer: payer,
+      user: payer,
+      memeTokensOut: new BN(1000),
+      quoteAmountIn: new BN(100000),
+      quoteMint: MEMECHAN_QUOTE_TOKEN.mint,
+      pool: boundPool.id,
+    });
+
+    tickets.push(new MemeTicket(ticketId2.id, client));
+    console.log("swapY ticketId2: " + ticketId2.id.toBase58());
 
     const boundPoolInfo = await BoundPoolClient.fetch2(client.connection, boundPool.id);
 
@@ -55,32 +81,6 @@ describe("StakingPool", () => {
     });
     console.log("ammPool: " + JSON.stringify(ammPool));
 
-    const tickets: MemeTicket[] = [];
-
-    const ticketId = await boundPool.swapY({
-      payer: payer,
-      user: payer,
-      memeTokensOut: new BN(1000),
-      quoteAmountIn: new BN(100000),
-      quoteMint: MEMECHAN_QUOTE_TOKEN.mint,
-      pool: boundPool.id,
-    });
-
-    tickets.push(new MemeTicket(ticketId.id, client));
-    console.log("swapY ticketId: " + ticketId.id.toBase58());
-
-    const ticketId2 = await boundPool.swapY({
-      payer: payer,
-      user: payer,
-      memeTokensOut: new BN(1000),
-      quoteAmountIn: new BN(100000),
-      quoteMint: MEMECHAN_QUOTE_TOKEN.mint,
-      pool: boundPool.id,
-    });
-
-    tickets.push(new MemeTicket(ticketId2.id, client));
-    console.log("swapY ticketId2: " + ticketId2.id.toBase58());
-
     const inputToken = MEMECHAN_QUOTE_TOKEN;
     const outputToken = new Token(TOKEN_PROGRAM_ID, ammPool.baseMint, MEMECHAN_MEME_TOKEN_DECIMALS)
     const inputTokenAmount = new TokenAmount(inputToken, 10000)
@@ -99,8 +99,11 @@ describe("StakingPool", () => {
 
     console.log('amm swapresult txids: ', swapTxIds)
 
+
+    StakingPool.fromStakingPoolId({ client, poolAccountAddressId: stakingPool.id});
+
     const unstakeResult = await stakingPool.unstake({
-      amount: new BN(10),
+      amount: new BN(1),
       user: payer,
       ticket: tickets[0],
     });
