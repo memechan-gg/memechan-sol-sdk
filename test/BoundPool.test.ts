7d77c35a16b93f51ec2f987171d2914d191e2ef2
Tamas Szucs <0xunreal@proton.me>
2024-05-24 14:09:17 +0200

    Update BoundPool.test.ts

--- test/BoundPool.test.ts
+++ test/BoundPool.test.ts
@@ -36,7 +36,6 @@ describe("BoundPool", () => {
     const boundPool = await BoundPoolClient.new({
       admin,
       payer,
-      signer: payer,
       client,
       quoteToken: MEMECHAN_QUOTE_TOKEN,
       tokenMetadata: DUMMY_TOKEN_METADATA,
@@ -84,7 +83,7 @@ describe("BoundPool", () => {
   }, 550000);
 
 
-  it.skip("init staking pool then go live", async () => {
+  it("init staking pool then go live", async () => {
     console.log("payer: " + payer.publicKey.toString());
     const pool = await BoundPoolClient.new({
       admin,
@@ -123,7 +122,7 @@ describe("BoundPool", () => {
 
     await sleep(2000);
 
-    const [stakingPool, ammPool] = await pool.goLive({
+    const [stakingPool, livePool ] = await pool.goLive({
       payer: payer,
       user: payer,
       boundPoolInfo,
@@ -132,194 +131,9 @@ describe("BoundPool", () => {
       quoteVault: stakingQuoteVault,
     });
 
+    const ammPool = livePool.ammPoolInfo;
     console.log("ammPool: " + JSON.stringify(ammPool));
 
     console.log("golive finished. stakingPool: " + stakingPool.id.toString() + " ammPool: " + ammPool.id.toString());
   }, 300000);
-
-  //   sleep(1000);
-
-  //   const poolInfo = await pool.fetch();
-
-  //   expect(poolInfo.locked).toBe(true);
-
-  //   const ticketInfo = await ticketId.fetch();
-
-  //   const memesTotal = ticketInfo.amount.add(poolInfo.adminFeesMeme);
-  //   expect(memesTotal).toBe(new BN(9e14)); //"total sum of memetokens with fees should amount to 9e14")
-
-  //   const solAmt = poolInfo.solReserve.tokens;
-  //   expect(solAmt).toBe(new BN(1 * 1e9)); // "pool should have 300 sol")
-
-  //   const solVault = await client.connection.getAccountInfo(
-  //     poolInfo.solReserve.vault,
-  //   )
-
-  //   const totalAmt = new BN(solVault?.lamports) - poolInfo.adminFeesSol;
-  //   expect(totalAmt).toBe(BigInt(2e11)); //, "pool should have 2 sol without admin fees")
-  // }, 120000);
-
-  // it("user swaps more than have", async () => {
-  //   const boundPool = await BoundPoolClient.new({admin, payer, signer: payer, client });
-  //   await sleep(1000);
-
-  //   const user = new Keypair();
-
-  //  await expect(boundPool.swapY({
-  //   memeTokensOut: new BN(1),
-  //   user: user,
-  //   solAmountIn: new BN(50.5 * 1e9),
-  //   userSolAcc: await createWrappedNativeAccount(
-  //     client.connection,
-  //     payer,
-  //     user.publicKey,
-  //     5 * 1e9
-  //   )
-  // })).rejects.toThrow();
-  // }, 120000);
-
-  // it("merge tickets presale", async () => {
-  //   const user = Keypair.generate()
-  //   await airdrop(client.connection, user.publicKey)
-  //   const pool = await BoundPoolClient.new({admin, payer, signer: payer, client });
-
-  //   await sleep(1000);
-
-  //   const userSolAcc = await createWrappedNativeAccount(
-  //     client.connection,
-  //     payer,
-  //     user.publicKey,
-  //     500 * 10e9
-  //   );
-
-  //   const tickets: MemeTicket[] = [];
-
-  //   tickets.push(await pool.swapY({
-  //     payer: payer,
-  //     user,
-  //     userSolAcc,
-  //     memeTokensOut: new BN(1),
-  //     solAmountIn: new BN(50.5 * 1e9),
-  //   }));
-
-  //   tickets.push(await pool.swapY({
-  //     payer: payer,
-  //     user,
-  //     userSolAcc,
-  //     memeTokensOut: new BN(1),
-  //     solAmountIn: new BN(70.7 * 1e9),
-  //   }));
-
-  //   tickets.push(await pool.swapY({
-  //     payer: payer,
-  //     user,
-  //     userSolAcc,
-  //     memeTokensOut: new BN(1),
-  //     solAmountIn: new BN(181.8 * 1e9),
-  //   }));
-
-  //   await tickets[0].boundMerge({
-  //     pool: pool.id,
-  //     ticketToMerge: tickets[1],
-  //     user: user
-  //   })
-
-  //   await tickets[0].boundMerge({
-  //     pool: pool.id,
-  //     ticketToMerge: tickets[2],
-  //     user: user
-  //   })
-  //   sleep(1000);
-  // }, 120000);
-
-  // it("swaps user sol->memecoin->sol", async () => {
-  //   const user = Keypair.generate()
-  //   await airdrop(client.connection, user.publicKey)
-  //   const pool = await BoundPoolClient.new({admin, payer, signer: payer, client });
-
-  //   const userSolAcc = await createWrappedNativeAccount(
-  //     client.connection,
-  //     payer,
-  //     user.publicKey,
-  //     500 * 10e9
-  //   );
-
-  //   await sleep(1000);
-
-  //   const ticketId = await pool.swapY({
-  //     user,
-  //     memeTokensOut: new BN(1),
-  //     solAmountIn: new BN(30 * 1e9),
-  //     userSolAcc
-  //   });
-
-  //   await sleep(6000);
-
-  //   await pool.swapX({
-  //     user,
-  //     userMemeTicket: ticketId,
-  //     userSolAcc
-  //   })
-  // }, 120000);
-
-  // it("swaps sol->memecoin->sol->full meme", async () => {
-  //   const user = Keypair.generate()
-  //   await airdrop(client.connection, user.publicKey)
-  //   const pool = await BoundPoolClient.new({admin, payer, signer: payer, client });
-
-  //   const userSolAcc = await createWrappedNativeAccount(
-  //     client.connection,
-  //     payer,
-  //     user.publicKey,
-  //     500 * 10e9
-  //   );
-
-  //   await sleep(1000);
-
-  //   const userMemeTicket = await pool.swapY({
-  //     payer: payer,
-  //     user,
-  //     memeTokensOut: new BN(1),
-  //     solAmountIn: new BN(30 * 1e9),
-  //     userSolAcc
-  //   });
-
-  //   await sleep(3000);
-
-  //   await pool.swapX({
-  //     user,
-  //     userMemeTicket,
-  //     userSolAcc
-  //   })
-
-  //   const ticketId = await pool.swapY({
-  //     payer: payer,
-  //     user: user,
-  //     memeTokensOut: new BN(1),
-  //     solAmountIn: new BN(303 * 1e9),
-  //   });
-
-  //   await sleep(1000);
-
-  //   const poolInfo = await pool.fetch();
-
-  //   expect(poolInfo.locked).toBe(true) //"pool should be locked")
-
-  //   const ticketOneInfo = await userMemeTicket.fetch();
-  //   const ticketInfo = await ticketId.fetch();
-
-  //   const memesTotal = ticketInfo.amount.add(ticketOneInfo.amount).add(poolInfo.adminFeesMeme);
-  //   expect(memesTotal).toBe(new BN(9e14)) // "total sum of memetokens with fees should amount to 9e14")
-
-  //   const solAmt = poolInfo.solReserve.tokens;
-  //   expect(solAmt.toBe(new BN(3e11))); // "pool should have 300 sol")
-
-  //   const solVault = await getAccount(
-  //     client.connection,
-  //     poolInfo.solReserve.vault,
-  //   )
-
-  //   const totalAmt = solVault.amount - BigInt(poolInfo.adminFeesSol.toNumber());
-  //   expect(totalAmt).toBe(BigInt(3e11)); // "pool should have 300 sol without admin fees")
-  // }, 120000);
 });
