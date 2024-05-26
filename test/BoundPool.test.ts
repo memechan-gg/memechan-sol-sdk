99aea6c500e08f9e97f2d7f1a308bfe95b2838a3
0xunreal <0xunreal@proton.me>
2024-05-26 17:48:15 +0200

    Update BoundPool.test.ts

--- test/BoundPool.test.ts
+++ test/BoundPool.test.ts
@@ -20,19 +20,65 @@ describe("BoundPool", () => {
     console.log(info);
   }, 150000);
 
+  it.skip("fails to create bound pool with invalid names", async () => {
+    const invalidNames = [
+      "ThisNameIsWayTooLongToBeValidAndShouldFail", // Exceeds 32 bytes
+    ];
+
+    for (const invalidName of invalidNames) {
+      const metadata = {
+        ...DUMMY_TOKEN_METADATA,
+        name: invalidName,
+      };
+
+      await expect(
+        BoundPoolClient.new({
+          admin,
+          payer,
+          client,
+          quoteToken: MEMECHAN_QUOTE_TOKEN,
+          tokenMetadata: metadata,
+        }),
+      ).rejects.toThrow();
+    }
+  }, 150000);
+
+  it.skip("fails to create bound pool with invalid symbol", async () => {
+    const invalidSymbols = [
+      "TOOLONGSYMBOL", // Exceeds 10 bytes
+    ];
+
+    for (const invalidSymbol of invalidSymbols) {
+      const metadata = {
+        ...DUMMY_TOKEN_METADATA,
+        symbol: invalidSymbol,
+      };
+
+      await expect(
+        BoundPoolClient.new({
+          admin,
+          payer,
+          client,
+          quoteToken: MEMECHAN_QUOTE_TOKEN,
+          tokenMetadata: metadata,
+        }),
+      ).rejects.toThrow();
+    }
+  }, 150000);
+
   it.skip("all", async () => {
     const all = await BoundPoolClient.all(client.memechanProgram);
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     for (const pool of all) {
-      //console.log(pool.account);
-      //console.log("==================================================");
+      // console.log(pool.account);
+      // console.log("==================================================");
     }
 
     console.log(all);
   }, 30000);
 
-  it("swapy, golive, should fail below tresholds", async () => {
+  it.skip("swapy, golive, should fail below tresholds", async () => {
     const boundPool = await BoundPoolClient.new({
       admin,
       payer,
@@ -41,7 +87,7 @@ describe("BoundPool", () => {
       tokenMetadata: DUMMY_TOKEN_METADATA,
     });
 
-    console.log("==== pool id: " + boundPool.id.toString());
+    console.log("==== pool id: " + boundPool.id.toString() + ", " + new Date().toUTCString());
     await sleep(2000);
 
     const tickets: MemeTicketClient[] = [];
@@ -50,41 +96,43 @@ describe("BoundPool", () => {
       payer: payer,
       user: payer,
       memeTokensOut: new BN(100 * 1e6),
-      quoteAmountIn: new BN(5000 * 1e9),
+      quoteAmountIn: new BN(500 * 1e9),
       quoteMint: MEMECHAN_QUOTE_TOKEN.mint,
       pool: boundPool.id,
     });
 
     tickets.push(new MemeTicketClient(ticketId.id, client));
-    console.log("swapY ticketId: " + ticketId.id.toBase58());
+    console.log("swapY ticketId: " + ticketId.id.toBase58() + ", " + new Date().toUTCString());
 
     const ticketId2 = await boundPool.swapY({
       payer: payer,
       user: payer,
       memeTokensOut: new BN(100 * 1e6),
-      quoteAmountIn: new BN(3499 * 1e9),
+      quoteAmountIn: new BN(499 * 1e9),
       quoteMint: MEMECHAN_QUOTE_TOKEN.mint,
       pool: boundPool.id,
     });
 
     tickets.push(new MemeTicketClient(ticketId2.id, client));
-    console.log("swapY ticketId2: " + ticketId2.id.toBase58());
+    console.log("swapY ticketId2: " + ticketId2.id.toBase58() + ", " + new Date().toUTCString());
 
     const boundPoolInfo = await BoundPoolClient.fetch2(client.connection, boundPool.id);
 
     console.log("boundPoolInfo:", boundPoolInfo);
 
-    await expect(
-      boundPool.initStakingPool({
-        payer: payer,
-        user: payer,
-        boundPoolInfo,
-      }),
-    ).rejects.toThrow();
+    // await expect(
+    const result = await boundPool.initStakingPool({
+      payer: payer,
+      user: payer,
+      boundPoolInfo,
+    });
+    // ).rejects.toThrow();
+
+    console.log("initStakingPool result: " + result + ", " + new Date().toUTCString());
   }, 550000);
 
-
   it.skip("init staking pool then go live", async () => {
+    console.log(" init staking pool then go live. " + new Date().toUTCString());
     console.log("payer: " + payer.publicKey.toString());
     const pool = await BoundPoolClient.new({
       admin,
@@ -111,7 +159,6 @@ describe("BoundPool", () => {
     const boundPoolInfo = await BoundPoolClient.fetch2(client.connection, pool.id);
 
     console.log("boundPoolInfo:", boundPoolInfo);
-
     const { stakingMemeVault, stakingQuoteVault } = await pool.initStakingPool({
       payer: payer,
       user: payer,
@@ -123,6 +170,8 @@ describe("BoundPool", () => {
 
     await sleep(2000);
 
+    console.log("golive start. " + new Date().toUTCString());
+
     const [stakingPool, livePool] = await pool.goLive({
       payer: payer,
       user: payer,
@@ -132,9 +181,70 @@ describe("BoundPool", () => {
       quoteVault: stakingQuoteVault,
     });
 
+    console.log("golive end. " + new Date().toUTCString());
+
     const ammPool = livePool.ammPoolInfo;
     console.log("ammPool: " + JSON.stringify(ammPool));
 
     console.log("golive finished. stakingPool: " + stakingPool.id.toString() + " ammPool: " + ammPool.id.toString());
   }, 300000);
+
+  it.skip("init staking pool, many swapy, then go live", async () => {
+    console.log(" init staking pool, many swapy then go live. " + new Date().toUTCString());
+    console.log("payer: " + payer.publicKey.toString());
+    const pool = await BoundPoolClient.new({
+      admin,
+      payer,
+      client,
+      quoteToken: MEMECHAN_QUOTE_TOKEN,
+      tokenMetadata: DUMMY_TOKEN_METADATA,
+    });
+
+    console.log("==== pool id: " + pool.id.toString());
+    await sleep(2000);
+
+    for (let i = 0; i < 20; i++) {
+      const ticketId = await pool.swapY({
+        payer: payer,
+        user: payer,
+        memeTokensOut: new BN(100 * 1e6),
+        quoteAmountIn: new BN(1000 * 1e9),
+        quoteMint: MEMECHAN_QUOTE_TOKEN.mint,
+        pool: pool.id,
+      });
+
+      console.log("swapY (" + i + ") ticketId: " + ticketId.id.toBase58());
+    }
+    const boundPoolInfo = await BoundPoolClient.fetch2(client.connection, pool.id);
+
+    console.log("boundPoolInfo:", boundPoolInfo);
+    const { stakingMemeVault, stakingQuoteVault } = await pool.initStakingPool({
+      payer: payer,
+      user: payer,
+      boundPoolInfo,
+    });
+
+    console.log("stakingMemeVault: " + stakingMemeVault.toString());
+    console.log("stakingQuoteVault: " + stakingQuoteVault.toString());
+
+    await sleep(2000);
+
+    console.log("golive start. " + new Date().toUTCString());
+
+    const [stakingPool, livePool] = await pool.goLive({
+      payer: payer,
+      user: payer,
+      boundPoolInfo,
+      feeDestinationWalletAddress: FEE_DESTINATION_ID,
+      memeVault: stakingMemeVault,
+      quoteVault: stakingQuoteVault,
+    });
+
+    console.log("golive end. " + new Date().toUTCString());
+
+    const ammPool = livePool.ammPoolInfo;
+    console.log("ammPool: " + JSON.stringify(ammPool));
+
+    console.log("golive finished. stakingPool: " + stakingPool.id.toString() + " ammPool: " + ammPool.id.toString());
+  }, 1500000);
 });
