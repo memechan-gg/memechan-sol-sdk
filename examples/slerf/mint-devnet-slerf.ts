94be571fee76b0f8b57cca9a8377bc93ad9b63be
0xunreal <0xunreal@proton.me>
2024-05-26 21:33:46 +0200

    Update mint-devnet-slerf.ts

--- examples/slerf/mint-devnet-slerf.ts
+++ examples/slerf/mint-devnet-slerf.ts
@@ -4,15 +4,15 @@ import { MintUtils } from "../../src/token/mintUtils";
 
 // yarn tsx examples/slerf/mint-devnet-slerf.ts > mint.txt 2>&1
 export const mintSlerf = async () => {
-    console.log("payer: " + payer.publicKey.toString());
-    console.log("quote token to mint: " + MEMECHAN_QUOTE_TOKEN.mint.toString());
+  console.log("payer: " + payer.publicKey.toString());
+  console.log("quote token to mint: " + MEMECHAN_QUOTE_TOKEN.mint.toString());
 
-    const mintUtils = new MintUtils(client.connection, payer);
+  const mintUtils = new MintUtils(client.connection, payer);
 
-    const getAccount1 = await mintUtils.getOrCreateTokenAccount(MEMECHAN_QUOTE_TOKEN.mint, payer, payer.publicKey );
-    console.log("getOrCreateTokenAccount getAccount1", getAccount1);
+  const getAccount1 = await mintUtils.getOrCreateTokenAccount(MEMECHAN_QUOTE_TOKEN.mint, payer, payer.publicKey);
+  console.log("getOrCreateTokenAccount getAccount1", getAccount1);
 
-    await mintUtils.mintTo(MEMECHAN_QUOTE_TOKEN.mint, getAccount1.address);
+  await mintUtils.mintTo(MEMECHAN_QUOTE_TOKEN.mint, getAccount1.address);
 };
 
 mintSlerf();
