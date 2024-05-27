596c95f3c9ceb807a08c447dfbf6e760d34b5786
0xUnreal <0xunreal@proton.me>
2024-05-27 18:05:47 +0200

    more tests

--- test/BoundPool.test.ts
+++ test/BoundPool.test.ts
@@ -4,6 +4,7 @@ import { sleep } from "../src/common/helpers";
 import { DUMMY_TOKEN_METADATA, admin, client, payer } from "./common/common";
 import { FEE_DESTINATION_ID, MEMECHAN_QUOTE_TOKEN } from "../src/config/config";
 import { MemeTicketClient } from "../src/memeticket/MemeTicketClient";
+import { PublicKey } from "@solana/web3.js";
 
 describe("BoundPool", () => {
   it.skip("creates bound pool", async () => {
@@ -130,7 +131,7 @@ describe("BoundPool", () => {
     console.log("initStakingPool result: " + result + ", " + new Date().toUTCString());
   }, 550000);
 
-  it.skip("init staking pool then go live", async () => {
+  it("init staking pool then go live", async () => {
     console.log(" init staking pool then go live. " + new Date().toUTCString());
     console.log("payer: " + payer.publicKey.toString());
     const pool = await BoundPoolClient.new({
@@ -154,6 +155,8 @@ describe("BoundPool", () => {
     });
 
     console.log("swapY ticketId: " + ticketId.id.toBase58());
+
+    await sleep(20000);
 
     const boundPoolInfo = await BoundPoolClient.fetch2(client.connection, pool.id);
 
@@ -186,7 +189,7 @@ describe("BoundPool", () => {
     console.log("ammPool: " + JSON.stringify(ammPool));
 
     console.log("golive finished. stakingPool: " + stakingPool.id.toString() + " ammPool: " + ammPool.id.toString());
-  }, 300000);
+  }, 500000);
 
   it.skip("init staking pool, many swapy, then go live", async () => {
     console.log(" init staking pool, many swapy then go live. " + new Date().toUTCString());
@@ -202,12 +205,12 @@ describe("BoundPool", () => {
     console.log("==== pool id: " + pool.id.toString());
     await sleep(2000);
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 10; i++) {
       const ticketId = await pool.swapY({
         payer: payer,
         user: payer,
         memeTokensOut: new BN(100 * 1e6),
-        quoteAmountIn: new BN(1000 * 1e9),
+        quoteAmountIn: new BN(41000 * 1e9),
         quoteMint: MEMECHAN_QUOTE_TOKEN.mint,
         pool: pool.id,
       });
@@ -246,4 +249,154 @@ describe("BoundPool", () => {
 
     console.log("golive finished. stakingPool: " + stakingPool.id.toString() + " ammPool: " + ammPool.id.toString());
   }, 1500000);
+});
+
+
+describe.skip('BoundPoolClient Tests 2', () => {
+  it('sells meme tokens', async () => {
+    const poolAccountAddressId = new PublicKey('8i4rzVFKhJobkF2RadFib4kBoLYYgWQoiVJnYzsMwA3X');
+    const boundPoolInstance = await BoundPoolClient.fromBoundPoolId({ client, poolAccountAddressId });
+
+    const inputAmount = '567.023231';
+    const minOutputAmount = await boundPoolInstance.getOutputAmountForSellMeme({
+      inputAmount: inputAmount,
+      slippagePercentage: 0,
+    });
+
+    console.debug('minOutputAmount: ', minOutputAmount);
+    const res = await boundPoolInstance.sellMeme({
+      inputAmount: inputAmount,
+      minOutputAmount: minOutputAmount,
+      slippagePercentage: 0,
+      user: payer.publicKey,
+      signer: payer,
+    });
+
+    expect(res).toBeDefined();
+  }, 150000);
+
+  it('buys meme tokens', async () => {
+    const poolAccountAddressId = new PublicKey('8i4rzVFKhJobkF2RadFib4kBoLYYgWQoiVJnYzsMwA3X');
+    const boundPoolInstance = await BoundPoolClient.fromBoundPoolId({ client, poolAccountAddressId });
+
+    const inputAmount = '0.568';
+    const minOutputAmount = await boundPoolInstance.getOutputAmountForBuyMeme({
+      inputAmount: inputAmount,
+      slippagePercentage: 0,
+    });
+
+    console.debug('minOutputAmount: ', minOutputAmount);
+
+    const res = await boundPoolInstance.buyMeme({
+      inputAmount: inputAmount,
+      minOutputAmount: minOutputAmount,
+      slippagePercentage: 0,
+      user: payer.publicKey,
+      signer: payer,
+    });
+
+    console.debug('res: ');
+    console.dir(res, { depth: null });
+
+    expect(res).toBeDefined();
+  }, 150000);
+
+  describe.skip('Edge Cases', () => {
+    it('handles zero input amount for sellMeme', async () => {
+      const poolAccountAddressId = new PublicKey('8i4rzVFKhJobkF2RadFib4kBoLYYgWQoiVJnYzsMwA3X');
+      const boundPoolInstance = await BoundPoolClient.fromBoundPoolId({ client, poolAccountAddressId });
+
+      const inputAmount = '0';
+      const minOutputAmount = await boundPoolInstance.getOutputAmountForSellMeme({
+        inputAmount: inputAmount,
+        slippagePercentage: 0,
+      });
+
+      await expect(
+        boundPoolInstance.sellMeme({
+          inputAmount: inputAmount,
+          minOutputAmount: minOutputAmount,
+          slippagePercentage: 0,
+          user: payer.publicKey,
+          signer: payer,
+        })
+      ).rejects.toThrow();
+    }, 150000);
+
+    it('handles negative input amount for buyMeme', async () => {
+      const poolAccountAddressId = new PublicKey('8i4rzVFKhJobkF2RadFib4kBoLYYgWQoiVJnYzsMwA3X');
+      const boundPoolInstance = await BoundPoolClient.fromBoundPoolId({ client, poolAccountAddressId });
+
+      const inputAmount = '-0.568';
+    //  await expect(
+      const amount = await  boundPoolInstance.getOutputAmountForBuyMeme({
+          inputAmount: inputAmount,
+          slippagePercentage: 0,
+        })
+        console.log('amount: ', amount);
+     // ).rejects.toThrow('Input amount must be a positive number');
+    }, 150000);
+
+    it('handles high slippage percentage for sellMeme', async () => {
+      const poolAccountAddressId = new PublicKey('8i4rzVFKhJobkF2RadFib4kBoLYYgWQoiVJnYzsMwA3X');
+      const boundPoolInstance = await BoundPoolClient.fromBoundPoolId({ client, poolAccountAddressId });
+
+      const inputAmount = '567.023231';
+      await expect(boundPoolInstance.getOutputAmountForSellMeme({
+        inputAmount: inputAmount,
+        slippagePercentage: 100,
+      })
+      ).rejects.toThrow("Slippage percentage must be between 0 (inclusive) and 100 (exclusive).");
+
+      await expect(boundPoolInstance.sellMeme({
+        inputAmount: inputAmount,
+        minOutputAmount: '5',
+        slippagePercentage: 100,
+        user: payer.publicKey,
+        signer: payer,
+      })
+      ).rejects.toThrow("Slippage percentage must be between 0 (inclusive) and 100 (exclusive).");
+
+    }, 150000);
+
+     it('too high slippage percentage for sellMeme', async () => {
+      const poolAccountAddressId = new PublicKey('8i4rzVFKhJobkF2RadFib4kBoLYYgWQoiVJnYzsMwA3X');
+      const boundPoolInstance = await BoundPoolClient.fromBoundPoolId({ client, poolAccountAddressId });
+
+      const inputAmount = '567.023231';
+      await expect(boundPoolInstance.getOutputAmountForSellMeme({
+        inputAmount: inputAmount,
+        slippagePercentage: 100,
+      })).rejects.toThrow("Slippage percentage must be between 0 (inclusive) and 100 (exclusive).");
+
+      await expect(boundPoolInstance.sellMeme({
+        inputAmount: inputAmount,
+        minOutputAmount: "5",
+        slippagePercentage: 100,
+        user: payer.publicKey,
+        signer: payer,
+      })).rejects.toThrow("Slippage percentage must be between 0 (inclusive) and 100 (exclusive).");
+    }, 150000);
+
+     it('handles high slippage percentage for sellMeme', async () => {
+      const poolAccountAddressId = new PublicKey('8i4rzVFKhJobkF2RadFib4kBoLYYgWQoiVJnYzsMwA3X');
+      const boundPoolInstance = await BoundPoolClient.fromBoundPoolId({ client, poolAccountAddressId });
+
+      const inputAmount = '567.023231';
+      const minOutputAmount = await boundPoolInstance.getOutputAmountForSellMeme({
+        inputAmount: inputAmount,
+        slippagePercentage: 99,
+      });
+
+      const res = await boundPoolInstance.sellMeme({
+        inputAmount: inputAmount,
+        minOutputAmount: minOutputAmount,
+        slippagePercentage: 99,
+        user: payer.publicKey,
+        signer: payer,
+      });
+
+      expect(res).toBeDefined();
+    }, 150000);
+  });
 });
