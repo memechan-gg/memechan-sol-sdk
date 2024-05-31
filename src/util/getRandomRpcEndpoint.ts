c38b2440bed77a15c7d54630a2b1353ff1803818
0xunreal <0xunreal@proton.me>
2024-05-31 02:07:54 +0200

    improve getRandomRpcEndpoint

--- src/util/getRandomRpcEndpoint.ts
+++ src/util/getRandomRpcEndpoint.ts
@@ -1,6 +1,6 @@
-import { endpoints } from "../../examples/load-balanced-connection/endpoints";
+import { endpoints as defaultEndpoints } from "../../examples/load-balanced-connection/endpoints";
 
-export function getRandomRpcEndpoint(): string {
+export function getRandomRpcEndpoint(endpoints = defaultEndpoints): string {
   const randomIndex = Math.floor(Math.random() * endpoints.length);
   return endpoints[randomIndex];
 }
