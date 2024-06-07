8865ae7925ae89560c71b8148a1758687c3b4d5f
avernikoz <avernikoz@gmail.com>
2024-06-07 16:10:05 +0300

    add legacy

---
+++ examples/meme-ticket/get-all-staked-lp-by-owner-legacy.ts
@@ -0,0 +1,14 @@
+import { PublicKey } from "@solana/web3.js";
+import { MemeTicketClient } from "../../src/memeticket/MemeTicketClient";
+import { client, payer } from "../common";
+
+// yarn tsx examples/meme-ticket/get-all-staked-lp-by-owner.ts > all-staked-lp.txt 2>&1
+export const getAllStakedLpByOwner = async () => {
+  const poolAddress = new PublicKey("6uM2Yf9VbkNndDutv6gxmvs2MsUVvpzLenSKEn7s46No");
+
+  const tickets = await MemeTicketClient.fetchTicketsByUser(poolAddress, client, payer.publicKey);
+
+  console.log("tickets:", tickets);
+};
+
+getAllStakedLpByOwner();
