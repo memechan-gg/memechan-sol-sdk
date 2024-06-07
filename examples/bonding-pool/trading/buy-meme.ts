602a5976eb04a9d514103baf24b8d8651d6fcb68
avernikoz <avernikoz@gmail.com>
2024-06-07 04:45:45 +0300

    upd buy-meme example

--- examples/bonding-pool/trading/buy-meme.ts
+++ examples/bonding-pool/trading/buy-meme.ts
@@ -4,10 +4,10 @@ import { client, payer } from "../../common";
 
 // yarn tsx examples/bonding-pool/trading/buy-meme.ts
 (async () => {
-  const poolAccountAddressId = new PublicKey("8T83gG397gcCaFh8hLiMECmhrmkLS7vtptMeLvCgwKde");
+  const poolAccountAddressId = new PublicKey("2ST4K87fWWYdeJ5SWyrv7bdSZmd3cYoqsthDeQZZn2TY");
   const boundPoolInstance = await BoundPoolClient.fromBoundPoolId({ client, poolAccountAddressId });
 
-  const inputAmount = "0.568";
+  const inputAmount = "12312";
 
   const minOutputAmount = await boundPoolInstance.getOutputAmountForBuyMeme({
     inputAmount: inputAmount,
@@ -22,6 +22,7 @@ import { client, payer } from "../../common";
     slippagePercentage: 0,
     user: payer.publicKey,
     signer: payer,
+    memeTicketNumber: 1,
   });
 
   console.debug("res: ");
