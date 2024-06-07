a3916c29b175edd5053f465b0049caf13d223fe7
avernikoz <avernikoz@gmail.com>
2024-06-07 16:22:51 +0300

    upd example

--- examples/meme-ticket/get-all-staked-lp-by-owner-legacy.ts
+++ examples/meme-ticket/get-all-staked-lp-by-owner-legacy.ts
@@ -2,7 +2,7 @@ import { PublicKey } from "@solana/web3.js";
 import { MemeTicketClient } from "../../src/memeticket/MemeTicketClient";
 import { client, payer } from "../common";
 
-// yarn tsx examples/meme-ticket/get-all-staked-lp-by-owner.ts > all-staked-lp.txt 2>&1
+// yarn tsx examples/meme-ticket/get-all-staked-lp-by-owner-legacy.ts > all-staked-lp.txt 2>&1
 export const getAllStakedLpByOwner = async () => {
   const poolAddress = new PublicKey("6uM2Yf9VbkNndDutv6gxmvs2MsUVvpzLenSKEn7s46No");
 
