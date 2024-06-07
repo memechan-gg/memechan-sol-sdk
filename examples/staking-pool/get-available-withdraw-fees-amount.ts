7cd5f6f67484f6b3f01a32dfa2b6bbac6e3efe27
0xUnreal <0xunreal@proton.me>
2024-06-07 13:39:46 +0200

    Update get-available-withdraw-fees-amount.ts

--- examples/staking-pool/get-available-withdraw-fees-amount.ts
+++ examples/staking-pool/get-available-withdraw-fees-amount.ts
@@ -1,28 +1,22 @@
-import { PublicKey, Transaction } from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
 import { StakingPool as CodegenStakingPool } from "../../src/schema/codegen/accounts";
 import { client, connection, payer } from "../common";
 import { MemeTicketClient, StakingPoolClient } from "../../src";
 
 // yarn tsx examples/staking-pool/get-available-withdraw-fees-amount.ts > withdraw-fees-amount.txt 2>&1
 export const getAvailableWithdrawFeesAmount = async () => {
-  const boundPoolId = new PublicKey("");
-
-  // Get staking pool
-  const stakingPoolAddress = new PublicKey("FMyKyPPmgRHdCVsfvdyuR6Y4BAgNvsQCPQtMAtEcysmm");
+  const boundPoolAddress = new PublicKey("2paxDkj5zFR3DtMVZtmTSbkMYZwFtVZnq2Xv1WFHqgPo");
+  const stakingPoolAddress = new PublicKey("sh5hozk6bENHvG4J5zrqW2S5eKjp68DRPZodCRLSkJU");
   const stakingPool = await StakingPoolClient.fromStakingPoolId({ client, poolAccountAddressId: stakingPoolAddress });
-  const ammPoolAddress = new PublicKey("3KgPp4q7DpeNSjA7aQSxtEKpDSGHDgf5SQasYUwd7A23");
 
   const fetchedStakingPool = await CodegenStakingPool.fetch(connection, stakingPoolAddress);
   console.log("fetchedStakingPool:", fetchedStakingPool?.toJSON());
 
-  // Get meme ticket
-  const memeTicketId = new PublicKey("2bEUpzNfkoeH6kFdDMxz2Q4MvVctFk8umumWunWHgPQt");
-  const memeTicket = new MemeTicketClient(memeTicketId, client);
-
+  const ammPoolAddress = stakingPool.raydiumAmm;
   // call addfees to accumulate fees
-  //await stakingPool.addFees({payer, transaction: new Transaction(), ammPoolId: ammPoolAddress});
+  await stakingPool.addFees({ payer, ammPoolId: ammPoolAddress });
 
-  const tickets = await MemeTicketClient.fetchTicketsByUser(boundPoolId, client, payer.publicKey);
+  const tickets = await MemeTicketClient.fetchTicketsByUser2(boundPoolAddress, client, payer.publicKey);
   const ticketFields = tickets.map((ticket) => ticket.fields);
 
   // Get available withdraw fees amount
