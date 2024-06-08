e8127ca7efe6acea8ca3bdff91d3a25c27975d7f
0xunreal <0xunreal@proton.me>
2024-06-08 11:51:12 +0200

    upd example

--- examples/bonding-pool/getHoldersMap.ts
+++ examples/bonding-pool/getHoldersMap.ts
@@ -6,7 +6,7 @@ import { BN } from "bn.js";
 // yarn tsx examples/bonding-pool/getHoldersMap.ts > getHoldersMap.txt 2>&1
 
 export async function getHoldersMap() {
-  const poolAddress = new PublicKey("3KzDGG71bzzZUApLNukyqVPZs4EJBd6SpJMh6ze6SDp2");
+  const poolAddress = new PublicKey("653jVw4EHQbcHRZBWXB3DVBkvkvS6kyRQhcvphoxEf36");
 
   const holdersMap = await BoundPoolClient.getHoldersMap(poolAddress, client);
   console.log("holdersMap:", holdersMap);
