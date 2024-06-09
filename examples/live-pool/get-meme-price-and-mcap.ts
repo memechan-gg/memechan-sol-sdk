1fe7538dad373d2dd24fc9edd15505611e2376fd
avernikoz <avernikoz@gmail.com>
2024-06-09 14:06:23 +0300

    upd examples

--- examples/live-pool/get-meme-price-and-mcap.ts
+++ examples/live-pool/get-meme-price-and-mcap.ts
@@ -4,7 +4,7 @@ import { connection } from "../common";
 
 // yarn tsx examples/live-pool/get-meme-price-and-mcap.ts > log.txt 2>&1
 export const getMemePriceAndMarketCap = async () => {
-  const poolAddress = "GvRAuPrbff7Pa95DqNMKnuGybMjt43nBJE4gMSKqtiiT";
+  const poolAddress = "9gZYYa6dPjEe1zHYTq2kd7c91UHsmdiRaVef1h8jKL5a";
   const memePrice = await LivePoolClient.getMemePrice({ poolAddress, connection, quotePriceInUsd: 0.31 });
   console.log("memePrice:", memePrice);
 
--- examples/target-config/change-target-config.ts
+++ examples/target-config/change-target-config.ts
@@ -7,15 +7,15 @@ import { PublicKey } from "@solana/web3.js";
 // yarn tsx examples/target-config/change-target-config.ts > change-target-config.txt 2>&1
 export const changeTargetConfig = async () => {
   // const targetConfigAddress = MEMECHAN_TARGET_CONFIG;
-  const targetConfigAddress = new PublicKey("CiXYgXYBwg9CvjYNcpjxjYViWbdDQVePhhZpWdwfEziN");
+  const targetConfigAddress = new PublicKey("5g13tz8GKWySjtzPRARuzzQM7LbMCUBMPGPef5PKe4JJ");
 
   const targetConfig = await TargetConfigClient.fromTargetConfigId({
     client,
     accountAddressId: targetConfigAddress,
   });
 
-  // const newTargetAmountRaw = 40000 * 10 ** MEMECHAN_QUOTE_TOKEN_DECIMALS;
-  const newTargetAmountRaw = 400 * 10 ** MEMECHAN_QUOTE_TOKEN_DECIMALS;
+  const newTargetAmountRaw = 40000 * 10 ** MEMECHAN_QUOTE_TOKEN_DECIMALS;
+  // const newTargetAmountRaw = 400 * 10 ** MEMECHAN_QUOTE_TOKEN_DECIMALS;
   const newTargetAmountBN = new BN(newTargetAmountRaw);
 
   console.log("original targetConfig: ", targetConfig.tokenTargetAmount.toString());
