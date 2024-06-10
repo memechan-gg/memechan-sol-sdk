c7a4f061577983d17c244d48cc16c3ccdaee30ec
0xUnreal <0xunreal@proton.me>
2024-06-10 14:56:13 +0200

    upd sample

--- examples/live-pool/get-meme-price-and-mcap.ts
+++ examples/live-pool/get-meme-price-and-mcap.ts
@@ -5,7 +5,7 @@ import { connection } from "../common";
 // yarn tsx examples/live-pool/get-meme-price-and-mcap.ts > log.txt 2>&1
 export const getMemePriceAndMarketCap = async () => {
   const poolAddress = "9gZYYa6dPjEe1zHYTq2kd7c91UHsmdiRaVef1h8jKL5a";
-  const memePrice = await LivePoolClient.getMemePrice({ poolAddress, connection, quotePriceInUsd: 0.31 });
+  const memePrice = await LivePoolClient.getMemePrice({ poolAddress, connection, quotePriceInUsd: 0.42956 });
   console.log("memePrice:", memePrice);
 
   const marketCap = BoundPoolClient.getMemeMarketCap({ memePriceInUsd: memePrice.priceInUsd });
