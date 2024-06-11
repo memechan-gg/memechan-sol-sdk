736b33695655b844097c08d4d4c76a44a8955c01
avernikoz <avernikoz@gmail.com>
2024-06-11 20:49:16 +0300

    add examples for pats hodlers

---
+++ examples/helius-api/get-pats-token-holders.ts
@@ -0,0 +1,19 @@
+import BigNumber from "bignumber.js";
+import { PATS_MINT } from "../../src/config/config";
+import { HeliusApiInstance } from "../common";
+import { saveDataToJsonFile } from "../utils";
+
+// yarn tsx examples/helius-api/get-pats-token-holders.ts
+export const getTokenHoldersExample = async () => {
+  const result = await HeliusApiInstance.getTokenHolders({ mint: PATS_MINT });
+  // console.debug("result.sortedByAmountList: ", result.sortedByAmountList);
+
+  saveDataToJsonFile(result.sortedByAmountList, "pats-holders-sorted-by-amount");
+
+  // Checks
+  const totalPatsAmount = result.sortedByAmountList.reduce((acc, el) => acc.plus(el.amountBN), new BigNumber(0));
+
+  console.debug("totalPatsAmount: ", totalPatsAmount.toString());
+};
+
+getTokenHoldersExample();
