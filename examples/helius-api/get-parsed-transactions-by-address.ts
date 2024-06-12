025439a0ca1b7d7f91edb6c17a6a12dd8d11070a
avernikoz <avernikoz@gmail.com>
2024-06-12 17:20:26 +0300

    update helius api examples

--- examples/helius-api/get-parsed-transactions-by-address.ts
+++ examples/helius-api/get-parsed-transactions-by-address.ts
@@ -9,14 +9,14 @@ import { saveDataToJsonFile } from "../utils";
     connection,
   });
 
-  console.debug("signatureData.txSignatureList: ", signatureData.txSignatureList);
+  // console.debug("signatureData.txSignatureList: ", signatureData.txSignatureList);
   console.debug("signatureData.txSignatureListSize: ", signatureData.txSignatureListSize);
 
   const { parsedDataList, parsedDataListSize } = await HeliusApiInstance.getAllParsedTransactions({
     signatures: signatureData.txSignatureList,
   });
 
-  console.debug("parsedDataList: ", parsedDataList);
+  // console.debug("parsedDataList: ", parsedDataList);
   console.debug("parsedDataListSize: ", parsedDataListSize);
 
   saveDataToJsonFile(parsedDataList, `tx-parsed-tx-data-for-pre-sale-address-${PRESALE_ADDRESS}`);
