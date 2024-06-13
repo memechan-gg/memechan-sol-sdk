7003a594b9fecddf85e8d7c872003ad13510c4d5
avernikoz <avernikoz@gmail.com>
2024-06-13 14:34:16 +0300

    upda programId in codegen & in idl

--- src/vesting/schema/codegen/programId.ts
+++ src/vesting/schema/codegen/programId.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
 // Program ID defined in the provided IDL. Do not edit, it will get overwritten.
-export const PROGRAM_ID_IDL = new PublicKey("4JDLXnMpPN129YHbGTwn16hW6xt2QgkfdYPMAgMQAcV5");
+export const PROGRAM_ID_IDL = new PublicKey("vestJGg7ZMQoXiAr2pLV5cqgtxFhEWzNoZL5Ngzb8H4");
 
 // This constant will not get overwritten on subsequent code generations and it's safe to modify it's value.
 export const PROGRAM_ID: PublicKey = PROGRAM_ID_IDL;
