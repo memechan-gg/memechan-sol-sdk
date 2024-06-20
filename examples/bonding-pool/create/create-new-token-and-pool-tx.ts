1d2d29032da61700d7e3d0d34789360887dc2542
0xUnreal <0xunreal@proton.me>
2024-06-20 14:47:49 +0200

    create pool example with initial buy meme

--- examples/bonding-pool/create/create-new-token-and-pool-tx.ts
+++ examples/bonding-pool/create/create-new-token-and-pool-tx.ts
@@ -2,7 +2,7 @@ import { sendAndConfirmTransaction } from "@solana/web3.js";
 import { BoundPoolClient } from "../../../src/bound-pool/BoundPoolClient";
 import { admin, payer, client, DUMMY_TOKEN_METADATA } from "../../common";
 import { getTxSize } from "../../../src/util/get-tx-size";
-import { TOKEN_INFOS, sleep } from "../../../src";
+import { MemeTicketClient, TOKEN_INFOS, sleep } from "../../../src";
 
 // yarn tsx examples/bonding-pool/create/create-new-token-and-pool-tx.ts > log.txt 2>&1
 export const createNewTokenAndPoolTx = async () => {
@@ -13,6 +13,13 @@ export const createNewTokenAndPoolTx = async () => {
       client,
       quoteToken: TOKEN_INFOS.WSOL,
       tokenMetadata: DUMMY_TOKEN_METADATA,
+      buyMemeTransactionArgs: {
+        inputAmount: "0.001",
+        minOutputAmount: "1",
+        slippagePercentage: 0,
+        user: payer.publicKey,
+        memeTicketNumber: MemeTicketClient.TICKET_NUMBER_START,
+      },
     });
 
   const memeMint = memeMintKeypair.publicKey;
