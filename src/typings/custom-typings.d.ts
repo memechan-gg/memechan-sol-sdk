a934edefe4709035b8081eb2f52061f8cb0dc596
harunskender97 <harun.skender@outlook.com>
2024-06-28 16:33:57 +0200

    test

---
+++ src/typings/custom-typings.d.ts
@@ -0,0 +1,11 @@
+declare module "@raydium-io/raydium-sdk" {
+  //   interface Token {}
+  //   export function jsonInfo2PoolKeys(e: any): void;
+  //   interface Percent {}
+  //   interface TokenAmount {}
+  //   interface METADATA_PROGRAM_ID {}
+  export const jsonInfo2PoolKeys: any;
+  export class Token {
+    constructor(id: any, e: any, e2: any);
+  }
+}
