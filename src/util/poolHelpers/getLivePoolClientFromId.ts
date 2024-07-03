94c817c90924c9bf650f0bcc2c0f1351e45fb91d
Harun <hskender@creditsights.com>
2024-07-03 13:35:36 +0200

    Update dynamic function for returning live pool

--- src/util/poolHelpers/getLivePoolClientFromId.ts
+++ src/util/poolHelpers/getLivePoolClientFromId.ts
@@ -15,8 +15,10 @@ export async function getLivePoolClientFromId(ammId: PublicKey, client: Memechan
   console.log("accountInfo.owner:", accountInfo.owner);
 
   if (accountInfo.owner.toBase58() == MEMECHAN_PROGRAM_ID_V2) {
-    return await LivePoolClientV2.fromAmmId(ammId, clientV2);
+    const pool = await LivePoolClientV2.fromAmmId(ammId, clientV2);
+    return { livePool: pool, version: "V2" };
   }
 
-  return await LivePoolClient.fromAmmId(ammId, client);
+  const pool = await LivePoolClient.fromAmmId(ammId, client);
+  return { livePool: pool, version: "V1" };
 }
