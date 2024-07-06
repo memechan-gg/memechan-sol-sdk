83052db3884ea73a8200494c108cd40eb359f587
0xunreal <0xunreal@proton.me>
2024-07-06 14:27:14 +0200

    Create get-quote-token-display-name.ts

---
+++ examples/v2/live-pool/get-quote-token-display-name.ts
@@ -0,0 +1,13 @@
+import { PublicKey } from "@solana/web3.js";
+import { LivePoolClientV2 } from "../../../src";
+import { clientV2 } from "../../common";
+
+// yarn tsx examples/v2/live-pool/get-quote-token-display-name.ts > log.txt 2>&1
+export const getQuoteTokenDisplayName = async () => {
+  const poolAddress = new PublicKey("69fywDppk1B9tXtGQJDiRKDcV8My6NLuyHgAmSToT6bZ");
+
+  const displayName = await LivePoolClientV2.getQuoteTokenDisplayName(poolAddress, clientV2);
+  console.log("displayName:", displayName);
+};
+
+getQuoteTokenDisplayName();
