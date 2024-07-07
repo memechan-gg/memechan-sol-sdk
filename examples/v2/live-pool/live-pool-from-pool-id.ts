e1e1c5b85f0dedbafecd828fc276d6cef0e11e72
0xunreal <0xunreal@proton.me>
2024-07-07 19:03:04 +0200

    Update live-pool-from-pool-id.ts

--- examples/v2/live-pool/live-pool-from-pool-id.ts
+++ examples/v2/live-pool/live-pool-from-pool-id.ts
@@ -5,7 +5,7 @@ import { getLivePoolClientFromId } from "../../../src";
 // yarn tsx examples/v2/live-pool/live-pool-from-pool-id.ts
 (async () => {
   const livePoolId = new PublicKey("69fywDppk1B9tXtGQJDiRKDcV8My6NLuyHgAmSToT6bZ");
-  const livePoolClient = getLivePoolClientFromId(livePoolId, client, clientV2);
+  const livePoolClient = await getLivePoolClientFromId(livePoolId, client, clientV2);
 
   console.debug("livePoolClient: ", livePoolClient);
 })();
