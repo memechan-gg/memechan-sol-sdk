78212425d3f8123b80a57d36fc53694ff22405a0
0xUnreal <0xunreal@proton.me>
2024-07-09 16:14:39 +0200

    fix import err

--- examples/v2/get-account-info.ts
+++ examples/v2/get-account-info.ts
@@ -1,9 +1,9 @@
 import { PublicKey } from "@solana/web3.js";
-import { connection } from "../../common";
+import { clientV2 } from "../common";
 
 // yarn tsx examples/v2/token-info/get-account-info.ts
 (async () => {
   const memeMint = new PublicKey("2Frbo28mMUYp3rNyuwA2cxyUPgZCEH8L41aRnyRjkHDi");
-  const info = await connection.getAccountInfo(memeMint);
+  const info = await clientV2.connection.getAccountInfo(memeMint);
   console.log(info);
 })();
