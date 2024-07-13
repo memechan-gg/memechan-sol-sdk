0b9bc7056cd97b3bd75528ece32a2819e913158d
0xunreal <0xunreal@proton.me>
2024-07-13 18:02:22 +0200

    v2 configs v2.1.97

--- src/schema/v2/codegen/programId.ts
+++ src/schema/v2/codegen/programId.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
 // Program ID defined in the provided IDL. Do not edit, it will get overwritten.
-export const PROGRAM_ID_IDL = new PublicKey("CaR9ciDnNnE6WX35tZWrjeGdKUPaft7r4oQGF4JhwVxZ");
+export const PROGRAM_ID_IDL = new PublicKey("chv2ogo3QopnnGctZM9Hxo2VCQ2zEcvQy6Uo6XVpiRH");
 
 // This constant will not get overwritten on subsequent code generations and it's safe to modify it's value.
 export const PROGRAM_ID: PublicKey = PROGRAM_ID_IDL;
