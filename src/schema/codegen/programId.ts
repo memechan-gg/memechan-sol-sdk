e99227f20b37192b81cde6c3ebe9ba54bf515bbd
0xunreal <0xunreal@proton.me>
2024-07-19 14:23:56 +0200

    fix codegen program ids

--- src/schema/codegen/programId.ts
+++ src/schema/codegen/programId.ts
@@ -1,7 +1,8 @@
 import { PublicKey } from "@solana/web3.js";
+import { MEMECHAN_PROGRAM_ID_PK } from "../../config/config";
 
 // Program ID defined in the provided IDL. Do not edit, it will get overwritten.
 export const PROGRAM_ID_IDL = new PublicKey("memeVtsr1AqAjfRzW2PuzymQdP2m7SgL6FQ1xgMc9MR");
 
 // This constant will not get overwritten on subsequent code generations and it's safe to modify it's value.
-export const PROGRAM_ID: PublicKey = PROGRAM_ID_IDL;
+export const PROGRAM_ID: PublicKey = MEMECHAN_PROGRAM_ID_PK;
--- src/schema/v2/codegen/programId.ts
+++ src/schema/v2/codegen/programId.ts
@@ -1,7 +1,8 @@
 import { PublicKey } from "@solana/web3.js";
+import { MEMECHAN_PROGRAM_ID_V2_PK } from "../../../config/config";
 
 // Program ID defined in the provided IDL. Do not edit, it will get overwritten.
 export const PROGRAM_ID_IDL = new PublicKey("chv2ogo3QopnnGctZM9Hxo2VCQ2zEcvQy6Uo6XVpiRH");
 
 // This constant will not get overwritten on subsequent code generations and it's safe to modify it's value.
-export const PROGRAM_ID: PublicKey = PROGRAM_ID_IDL;
+export const PROGRAM_ID: PublicKey = MEMECHAN_PROGRAM_ID_V2_PK;
