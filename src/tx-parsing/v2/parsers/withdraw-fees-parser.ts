ad89714f998a7fb66f767b70a7205fe3988f48ea
0xunreal <0xunreal@proton.me>
2024-07-23 09:54:52 +0200

    wip schemas

---
+++ src/tx-parsing/v2/parsers/withdraw-fees-parser.ts
@@ -0,0 +1,54 @@
+import { ParsedTransactionWithMeta, PublicKey } from "@solana/web3.js";
+import { CHAN_TOKEN_DECIMALS, MEMECHAN_MEME_TOKEN_DECIMALS, TOKEN_INFOS } from "../../../config/config";
+
+export type WithdrawFeesInstructionParsed = {
+  sender: PublicKey;
+  stakingPoolAddress: PublicKey;
+  feesMeme: number;
+  feesQuote: number;
+  feesChan: number;
+  type: "withdrawFees";
+};
+
+export async function parseWithdrawFeesInstruction(
+  tx: ParsedTransactionWithMeta,
+  index: number,
+): Promise<WithdrawFeesInstructionParsed | undefined> {
+  const ix = tx.transaction.message.instructions[index];
+
+  if (!("accounts" in ix)) {
+    return undefined;
+  }
+
+  const stakingPoolAddress = ix.accounts[0];
+
+  if (!tx.meta?.logMessages) {
+    return undefined;
+  }
+
+  const feesRegex = /fees_meme: (\d+) fees_quote: (\d+) fees_chan: (\d+)/;
+  for (const log of tx.meta.logMessages) {
+    const match = log.match(feesRegex);
+    if (match) {
+      const feesMeme = parseInt(match[1], 10) / 10 ** MEMECHAN_MEME_TOKEN_DECIMALS; // Convert to decimal with 6 places
+      const feesQuote = parseInt(match[2], 10) / 10 ** TOKEN_INFOS.WSOL.decimals; // Convert to decimal with 9 places // todo make it dynamic
+      const feesChan = parseInt(match[3], 10) / 10 ** CHAN_TOKEN_DECIMALS; // Convert to decimal with 9 places
+      console.log(
+        `fees_meme: ${feesMeme.toFixed(6)}, fees_quote: ${feesQuote.toFixed(9)}, chan: ${feesChan.toFixed(9)}`,
+      );
+
+      const withdrawFeesParsed: WithdrawFeesInstructionParsed = {
+        stakingPoolAddress: stakingPoolAddress,
+        feesChan: feesChan,
+        feesMeme: feesMeme,
+        feesQuote: feesQuote,
+        sender: tx.transaction.message.accountKeys[0].pubkey, // In the `Message` structure, the first account is always the fee-payer
+        type: "withdrawFees",
+      };
+
+      return withdrawFeesParsed;
+    }
+  }
+
+  return undefined;
+}
