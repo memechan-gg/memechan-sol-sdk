c726ccba9198db961b061e9c69cb6f835808bede
0xUnreal <0xunreal@proton.me>
2024-07-24 11:38:26 +0200

    fix codegen programids

--- src/schema/codegen/programId.ts
+++ src/schema/codegen/programId.ts
@@ -1,17 +1,13 @@
 import { PublicKey } from "@solana/web3.js";
 import dotenv from "dotenv";
-import devConfig from "../../config/config.dev";
-import prodConfig from "../../config/config.prod";
-// import { getEnvVar } from "../../config/config";
 
 dotenv.config();
-// const isProduction = process.env.NODE_ENV === "production";
-// const configSource = isProduction ? prodConfig : devConfig;
 
 // Program ID defined in the provided IDL. Do not edit, it will get overwritten.
-// export const PROGRAM_ID_IDL = new PublicKey("memeVtsr1AqAjfRzW2PuzymQdP2m7SgL6FQ1xgMc9MR");
+export const PROGRAM_ID_IDL = new PublicKey("memeVtsr1AqAjfRzW2PuzymQdP2m7SgL6FQ1xgMc9MR");
 
-// This constant will not get overwritten on subsequent code generations and it's safe to modify it's value.
-// TODO fix. This is a placeholder
-// export const PROGRAM_ID: PublicKey = getEnvVar("MEMECHAN_PROGRAM_ID_PK", configSource.MEMECHAN_PROGRAM_ID_PK);
-export const PROGRAM_ID: PublicKey = new PublicKey(process.env.MEMECHAN_PROGRAM_ID_PK!);
+// It might get overriden during code generation
+
+export const PROGRAM_ID: PublicKey = process.env.MEMECHAN_PROGRAM_ID
+  ? new PublicKey(process.env.MEMECHAN_PROGRAM_ID)
+  : PROGRAM_ID_IDL;
--- src/schema/v2/codegen/programId.ts
+++ src/schema/v2/codegen/programId.ts
@@ -1,8 +1,11 @@
 import { PublicKey } from "@solana/web3.js";
-import { MEMECHAN_PROGRAM_ID_V2_PK } from "../../../config/config";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Program ID defined in the provided IDL. Do not edit, it will get overwritten.
 export const PROGRAM_ID_IDL = new PublicKey("chv2ogo3QopnnGctZM9Hxo2VCQ2zEcvQy6Uo6XVpiRH");
 
-// This constant will not get overwritten on subsequent code generations and it's safe to modify it's value.
-export const PROGRAM_ID: PublicKey = MEMECHAN_PROGRAM_ID_V2_PK;
+export const PROGRAM_ID: PublicKey = process.env.MEMECHAN_PROGRAM_ID_V2
+  ? new PublicKey(process.env.MEMECHAN_PROGRAM_ID_V2)
+  : PROGRAM_ID_IDL;
