b342261d9db0ec311c858366107ebf699ad7a622
0xunreal <0xunreal@proton.me>
2024-08-20 16:06:26 +0200

    env vars

--- src/vechan-vesting/schema/codegen/programId.ts
+++ src/vechan-vesting/schema/codegen/programId.ts
@@ -3,6 +3,6 @@ import { PublicKey } from "@solana/web3.js";
 // This constant will not get overwritten on subsequent code generations and it's safe to modify it's value.
 const PROGRAM_ID_IDL: PublicKey = new PublicKey("vestJGg7ZMQoXiAr2pLV5cqgtxFhEWzNoZL5Ngzb8H4");
 
-export const PROGRAM_ID: PublicKey = process.env.VESTING_PROGRAM_ID
-  ? new PublicKey(process.env.VESTING_PROGRAM_ID)
+export const PROGRAM_ID: PublicKey = process.env.NEXT_PUBLIC_VESTING_PROGRAM_ID
+  ? new PublicKey(process.env.NEXT_PUBLIC_VESTING_PROGRAM_ID)
   : PROGRAM_ID_IDL;
